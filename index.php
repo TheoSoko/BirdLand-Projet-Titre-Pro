99b88ed9eb37a1117c207f309666b02c1e612062
Your Name <you@example.com>
2022-01-04 17:00:07 +0100

    .

--- index.php
+++ index.php
@@ -28,13 +28,13 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col d-flex justify-content-center">
-                <H1>BIRDLAND</H1>
+                <H1 id="h1mainTitle">BIRDLAND</H1>
             </div>
         </div>
         <div class="row">
             <div class="col d-flex justify-content-center">
                 <div class="presentation py-3">
-                    <h2>Présentation du site</h2>
+                    <h2 id="h2Presentation">Présentation du site</h2>
                     <p class="presentationText">Ellentesque et dolor tristique, eleifend turpis eget, laoreet ipsum. Proin commodo, risus sit amet varius laoreet, metus urna cursus dolor.
                         Donec fringilla vel libero id fringilla. Donec nec odio eget dolor fringilla venenatis. </p>
                     <p class="presentationText"> In sit amet arcu auctor, rhoncus lacus a, tincidunt magna. Duis tristique dignissim nunc, non interdum odio placerat id. </p>
@@ -47,12 +47,12 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-xl col-sm-12 d-flex justify-content-center"><span class="blocCollection text-center mx-2 mx-md-0"><a class="blocCategoryText" href="">COLLECTION</a></span></div>
-            <div class="col-xl col-sm-12 d-flex justify-content-center"><span class="blocArticles text-center mx-2 mx-md-0"><a class="blocCategoryText" href="">ARTICLES</a></span></div>
+            <div class="col-xl-6 col-sm-12 d-flex justify-content-center"><span class="blocCollection text-center"><a class="blocCategoryText" href="">COLLECTION</a></span></div>
+            <div class="col-xl-6 col-sm-12 d-flex justify-content-center"><span class="blocArticles text-center"><a class="blocCategoryText" href="">ARTICLES</a></span></div>
         </div>
         <div class="row">
-            <div class="col-xl col-sm-12 d-flex justify-content-center"><span class="blocMusiciens text-center mx-2 mx-md-0"><a class="blocCategoryText" href="">MUSICIENS</a></span></div>
-            <div class="col-xl col-sm-12 d-flex justify-content-center"><span class="blocPlaylists text-center mx-2 mx-md-0"><a class="blocCategoryText" href="">PLAYLISTS</a></span></div>
+            <div class="col-xl-6 col-sm-12 d-flex justify-content-center"><span class="blocMusiciens text-center"><a class="blocCategoryText" href="">MUSICIENS</a></span></div>
+            <div class="col-xl-6 col-sm-12 d-flex justify-content-center"><span class="blocPlaylists text-center"><a class="blocCategoryText" href="">PLAYLISTS</a></span></div>
         </div>
         <div class="row">
             <div class="col d-flex justify-content-center"><span class="blocCulture text-center mx-2 mx-md-0"><a class="blocCategoryText" href="">CULTURE</a></span></div>
